c73fbda91233bd17223ae6defdf3f582f2d681b7
Thambaru Wijesekara <thambaru@gmail.com>
2015-08-01 19:48:39 +0530

    Listen to global options.

--- jquery.stateful.js
+++ jquery.stateful.js
@@ -2,26 +2,40 @@
 Developed by thambaru (thambaru.com) under MIT License.
 */
 
-jQuery(document).ready(function($) {
+(function($) {
+    var opts = new Object(),
+        defaults = {
+            text: "Loading...",
+            delay: 3000,
+            disabled: false
+        };
+
+    $.fn.stateful = function(options) {
+        opts = $.extend(defaults, options);
+        return this;
+    };
+
     $('[data-loading-text]').click(function() {
         stateful($(this));
     });
-});
 
-function stateful(loader) {
-    var text = loader.attr('data-loading-text'),
-        delay = loader.attr('data-loading-delay'),
-        value = loader.html();
-    options = {
-        text: text === '' ? 'Loading...' : text,
-        delay: typeof delay === 'undefined' || delay === '' ? 3000 : delay,
-        value: value
-    };
-    changeStates(loader, options);
-}
+    function stateful(loader) {
+        var text = loader.attr('data-loading-text'),
+            delay = loader.attr('data-loading-delay'),
+            value = loader.html();
+        attrs = {
+            text: text === '' ? '' : text,
+            delay: typeof delay === 'undefined' || delay === '' ? 3000 : delay,
+            value: value
+        };
+        var options = $.extend(attrs, opts);
+        if (!options.disabled)
+            changeStates(loader, options);
+    }
 
-function changeStates(loader, options) {
-    loader.attr('disabled', true).addClass('disabled').html(options.text).delay(options.delay).queue(function() {
-        loader.attr('disabled', false).removeClass('disabled').html(options.value).dequeue();
-    });
-}
+    function changeStates(loader, options) {
+        loader.attr('disabled', true).addClass('disabled').html(options.text).delay(options.delay).queue(function() {
+            loader.attr('disabled', false).removeClass('disabled').html(options.value).dequeue();
+        });
+    }
+}(jQuery));
